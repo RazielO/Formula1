4c4d96bfa2b02279989eb5aa5b4b5da91c8475b1
RazielO <oraziel_a@hotmail.com>
2021-10-04 12:54:21 -0500

    Route: /season/round/background
    If the wiki background has children, they is show with its title

--- src/routes/[season([0-9]{4})]_[round([0-9]+)]/background.svelte
+++ src/routes/[season([0-9]{4})]_[round([0-9]+)]/background.svelte
@@ -14,6 +14,8 @@
     import { onMount } from "svelte";
     import { goto } from "@sapper/app";
 
+    import Loader from "../../components/Loader.svelte";
+
     import wtf from "wtf_wikipedia";
 
     import { rounds } from "../../stores.js";
@@ -22,11 +24,10 @@
 
     let season = data.season;
     let round = data.round;
-    let d = data.data;
 
-    let info;
     let name;
     let url;
+    let hasBackground;
 
     onMount(async () => {
         if ($rounds[season] === undefined) {
@@ -37,14 +38,35 @@
             name = `${season} ${$rounds[season][round].name}`;
 
             if ($rounds[season][round].background === undefined) {
+                let info = {};
+
                 await wtf.fetch(name).then((doc) => {
-                    info = doc.section("Background").text();
+                    hasBackground =
+                        doc.sections().filter((s) => s._title === "Background")
+                            .length !== 0;
+                    if (hasBackground) {
+                        let section = doc.section("Background");
+                        info["text"] = section.text();
+                        info["children"] = [];
+
+                        for (let i = 0; i < section.children().length; i++) {
+                            const element = section.children()[i];
+                            info["children"][i] = {
+                                title: element.json().title,
+                                text: element.text(),
+                            };
+                        }
+                    } else {
+                        info["text"] = doc.sections()[0].text();
+                        info["children"] = [];
+                    }
                     url = doc.url();
                 });
 
                 rounds.update((value) => {
                     value[season][round].background = {
                         info: info,
+                        hasBackground: hasBackground,
                         url: url,
                     };
                     return value;
@@ -58,10 +80,26 @@
     {#if $rounds[season][round] !== undefined}
         {#if $rounds[season][round].background !== undefined}
             <h1>{name}</h1>
+
             <h2>Background</h2>
 
-            {#each $rounds[season][round].background.info.split("\n") as paragraph}
+            {#if !$rounds[season][round].background.hasBackground}
+                <h3>
+                    The wikipedia site did not have a 'Background' section, so
+                    this is the general information of the GP.
+                </h3>
+            {/if}
+
+            {#each $rounds[season][round].background.info.text.split("\n") as paragraph}
                 <p>{paragraph}</p>
+            {/each}
+
+            {#each $rounds[season][round].background.info.children as child}
+                <h3>{child.title}</h3>
+
+                {#each child.text.split("\n") as paragraph}
+                    <p>{paragraph}</p>
+                {/each}
             {/each}
 
             <small>
@@ -69,6 +107,8 @@
                     href={$rounds[season][round].background.url}>wikipedia</a
                 >
             </small>
+        {:else}
+            <Loader />
         {/if}
     {/if}
 {/if}
