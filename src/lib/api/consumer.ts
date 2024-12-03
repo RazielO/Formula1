f03f8517862921efe89af3cecef512dae43916be
RazielO <oraziel_a@hotmail.com>
2024-12-02 22:28:39 -0600

    Moved API functions inside one file only

--- src/lib/api/consumer.ts
+++ src/lib/api/consumer.ts
@@ -1,19 +1,67 @@
-import * as seasons from "./consumers/seasons"
-import * as races from "./consumers/races"
-import * as qualy from "./consumers/qualyfing"
-import * as raceResults from "./consumers/raceResults"
-import * as sprintResults from "./consumers/sprintResults"
+import type { MRData } from "./types/MRData"
+import type { QualifyingResult } from "./types/QualifyingResult"
+import type { RaceResult } from "./types/RaceResult"
+import type { Race, RaceTable } from "./types/Races"
+import type { SeasonTable } from "./types/Seasons"
 
 export const BASE_URL = "https://api.jolpi.ca/ergast/f1"
 
 export class Consumer {
-    static getFirstSeason = async () => await seasons.getFirstSeason();
+    static async getFirstSeason(): Promise<number> {
+        const url = `${BASE_URL}/seasons.json?limit=1`
+        const response: { MRData: MRData } = await fetch(url).then(data => data.json());
 
-    static getRaces = async (season: number) => await races.getRaces(season);
+        if (response.MRData.SeasonTable !== undefined) {
+            const data: SeasonTable = response.MRData.SeasonTable;
+            return Number.parseInt(data.Seasons[0].season);
+        }
 
-    static getQualyResults = async (season: number, round: number) => await qualy.getQualyResults(season, round);
+        return 1950;
+    }
 
-    static getRaceResults = async (season: number, round: number) => await raceResults.getRaceResults(season, round);
 
-    static getSprintResults = async (season: number, round: number) => await sprintResults.getSprintResults(season, round);
+    static async getRaces(season: number): Promise<Race[]> {
+        let url = `${BASE_URL}/${season}.json`;
+        let response: { MRData: MRData } = await fetch(url).then((data) => data.json());
+
+        if (response.MRData.RaceTable !== undefined) {
+            const data: RaceTable = response.MRData.RaceTable;
+            return data.Races;
+        }
+
+        return [];
+    }
+
+    static async getQualyResults(season: number, round: number): Promise<QualifyingResult[]> {
+        let url = `${BASE_URL}/${season}/${round}/qualifying.json`;
+        let response: { MRData: MRData } = await fetch(url).then((data) => data.json());
+
+        if (response.MRData.RaceTable?.Races.length === 0) {
+            return [];
+        }
+
+        return response.MRData.RaceTable?.Races[0].QualifyingResults!;
+    }
+
+    static async getRaceResults(season: number, round: number): Promise<RaceResult[]> {
+        let url = `${BASE_URL}/${season}/${round}/results.json`;
+        let response: { MRData: MRData } = await fetch(url).then((data) => data.json());
+
+        if (response.MRData.RaceTable?.Races.length === 0) {
+            return [];
+        }
+
+        return response.MRData.RaceTable?.Races[0].Results!;
+    }
+
+    static async getSprintResults(season: number, round: number): Promise<RaceResult[]> {
+        let url = `${BASE_URL}/${season}/${round}/sprint.json`;
+        let response: { MRData: MRData } = await fetch(url).then((data) => data.json());
+
+        if (response.MRData.RaceTable?.Races.length === 0) {
+            return [];
+        }
+
+        return response.MRData.RaceTable?.Races[0].SprintResults!;
+    }
 }
